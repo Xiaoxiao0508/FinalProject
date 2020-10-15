48dca1b51b339e3ceecfbefc9d631b261b411b91
Xiaoxiao <cxxmelbourne@hotmail.com>
2020-10-15 22:12:24 +1100

    Data Dictionary&relational schema

---
+++ BuildDB.sql
@@ -0,0 +1,13 @@
+-- Relational schema
+-- - Player(UserName)
+--     Primary Key(UserName
+-- )
+-- Choice(ChoiceName)
+-- Primary Key (ChoiceName)
+
+-- Round(DateTime,UserName,ChoiceName)
+-- Primary Key (DataTime,UserName)
+-- Foreign Key (UserName)REFERENCES Player
+-- Foreign Key (ChoiceName)REFERENCES Choice
+
+
