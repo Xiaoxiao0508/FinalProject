5e2e7fd399f3041cd808870375fb99a727326509
Xiaoxiao <cxxmelbourne@hotmail.com>
2020-10-17 00:26:49 +1100

    navigate around,display choice on display page

---
+++ RockPaperScissor/src/app/routers/choice/choice.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ChoiceserveService } from 'src/app/choiceserve.service';
+
+@Component({
+  selector: 'app-choice',
+  templateUrl: './choice.component.html',
+  styleUrls: ['./choice.component.css']
+})
+export class ChoiceComponent implements OnInit {
+
+  constructor(private serve:ChoiceserveService) { }
+
+  ngOnInit(): void {
+  }
+  choice(option:'rock'|'paper'|'scissor'){
+   
+    this.serve.commitchoice(option)
+  }
+  shoot(){
+    this.serve.commitshoot();
+ 
+  }
+
+}
