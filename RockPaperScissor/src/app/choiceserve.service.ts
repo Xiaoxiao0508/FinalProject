92229ed04e91cb9b914f7eb7042c55efa4eac079
Xiaoxiao <cxxmelbourne@hotmail.com>
2020-10-17 00:25:37 +1100

    generate service

---
+++ RockPaperScissor/src/app/choiceserve.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, ɵNOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChoiceserveService {
+  private _choice: 'rock' | 'paper' | 'scissor';
+  get choice(){
+    return this._choice;
+  }
+  public Isrock:boolean=false;
+  public Ispaper:boolean=false;
+  public Isscissor:boolean=false;
+
+  constructor(private router:Router) { }
+  commitchoice(option: 'rock' | 'paper' | 'scissor') {
+    this._choice = option;  
+  };
+  commitshoot(){
+    if(this._choice=='rock'){
+      this.Isrock=true; 
+    }
+    if(this._choice=='paper'){
+      this.Ispaper=true; 
+    }
+    else if (this._choice=='scissor'){
+      this.Isscissor=true; 
+    }
+    alert('11111')
+    this.router.navigateByUrl('/display');
+    
+  }
+  committry(){
+    this.router.navigateByUrl("/choice");
+   this.Isrock=false;
+    this.Ispaper=false;
+    this.Isscissor=false;
+  }
+}
