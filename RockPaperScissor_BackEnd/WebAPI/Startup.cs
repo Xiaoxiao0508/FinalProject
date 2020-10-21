a7620bece864fc758596ed197ecb4caf9d3d45cf
Xiaoxiao <cxxmelbourne@hotmail.com>
2020-10-22 10:13:10 +1100

    modify the startup.cs for cross -origin request

--- RockPaperScissor_BackEnd/WebAPI/Startup.cs
+++ RockPaperScissor_BackEnd/WebAPI/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+// this file is modified for the cross-origin request purpose
 namespace RockPaperScissorsApi
 {
     public class Startup
